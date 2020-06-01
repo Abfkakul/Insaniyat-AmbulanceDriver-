4b843dead70771a7a9540429ba6225a1a64da351
AbfKakul <anasbinfaisal996@gmail.com>
2020-06-01 19:55:39 +0500

    Updates while running the app

--- app/src/main/java/com/example/insaniyatambulancedriver/MainActivity.java
+++ app/src/main/java/com/example/insaniyatambulancedriver/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.insaniyatambulancedriver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        Button Login = findViewById(R.id.login);
+        Button SignUp = findViewById(R.id.signup);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenLoginPage();
+            }
+        });
+
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenSignUpPage();
+            }
+        });
+
+    }
+
+    void OpenSignUpPage(){
+        Intent i = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(i);
+    }
+
+    void OpenLoginPage(){
+        Intent i = new Intent(MainActivity.this, SignInActivity.class);
+        startActivity(i);
     }
 }
